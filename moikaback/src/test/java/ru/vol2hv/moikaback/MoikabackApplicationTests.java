a4bc0763d1bb04144ff9d471566cbe3138e83127
vol2hv <vol2hv@yandex.ru>
2017-10-04 20:20:18 +0300

    Пустой модуль бэка

---
+++ moikaback/src/test/java/ru/vol2hv/moikaback/MoikabackApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.vol2hv.moikaback;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoikabackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
